d164aa3dd35925b9ce1cc08c49ac62f52e0ca946
Tristan Camejo <contact@tristancamejo.com>
2022-11-10 09:25:55 +1100

    feat: typescript example

---
+++ typescript/src/commands/hey.ts
@@ -0,0 +1,20 @@
+import { Command, type ChatInputInteraction } from "disploy";
+
+export default class HeyCommand extends Command {
+  public constructor() {
+    super({
+      name: "hey",
+      description: "hey!",
+    });
+  }
+
+  override async slashRun(interaction: ChatInputInteraction) {
+    interaction.deferReply();
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    return void interaction.editReply({
+      content: `Just wanted to say hey!`,
+    });
+  }
+}
